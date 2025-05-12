09ba8f4dd36327fc80c5890abdf1551bdd0b4aec
Brummell <brummell@MacBook-Tobi.local>
2025-05-12 09:37:15 +0100

    bug fix font heading

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <body className={`${inter.variable} ${raleway.variable} font-sans antialiased`}>
+      <body className={`${inter.variable} ${raleway.variable} font-sans antialiased`} suppressHydrationWarning data-testim-main-word-scripts-loaded="true">
         {children}
       </body>
     </html>
